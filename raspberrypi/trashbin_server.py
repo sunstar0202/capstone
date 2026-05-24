9a9a1c7cbec83d878d9a7bef90f3ddf8bda04cad
jws0206 <111034101+jws0206@users.noreply.github.com>
2026-05-24 10:29:17 +0900

    Create trashbin_server.py
    프로젝트 폴더안에 새로 raspberrypi 폴더 생성, 키트 실행 코드 넣어둠
    라즈베리파이키트 연결해서 터미널에  블루투스랑 연결에 필요한 파일들 설치해뒀고 잘 실행됌

---
+++ raspberrypi/trashbin_server.py
@@ -0,0 +1,77 @@
+Python 3.11.9 (tags/v3.11.9:de54cf5, Apr  2 2024, 10:12:12) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> ```python
+... import bluetooth
+... import RPi.GPIO as GPIO
+... import time
+... 
+... SERVO_PIN = 18
+... 
+... GPIO.setmode(GPIO.BCM)
+... GPIO.setup(SERVO_PIN, GPIO.OUT)
+... 
+... servo = GPIO.PWM(SERVO_PIN, 50)
+... servo.start(0)
+... 
+... 
+... def set_angle(angle):
+...     duty = 2 + (angle / 18)
+...     GPIO.output(SERVO_PIN, True)
+...     servo.ChangeDutyCycle(duty)
+...     time.sleep(0.5)
+...     GPIO.output(SERVO_PIN, False)
+...     servo.ChangeDutyCycle(0)
+... 
+... 
+... def open_lid():
+...     print("open")
+...     set_angle(90)
+...     time.sleep(3)
+... 
+... 
+... def close_lid():
+...     print("close")
+...     set_angle(0)
+...     time.sleep(1)
+... 
+... 
+... server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+... port = 1
+
+server_sock.bind(("", port))
+server_sock.listen(1)
+
+bluetooth.advertise_service(
+    server_sock,
+    "TrashBinServer",
+    service_classes=[bluetooth.SERIAL_PORT_CLASS],
+    profiles=[bluetooth.SERIAL_PORT_PROFILE]
+)
+
+print("Connecting...")
+
+client_sock, address = server_sock.accept()
+print(f"Connected: {address}")
+
+try:
+    while True:
+        data = client_sock.recv(1024).decode("utf-8").strip()
+
+        if data:
+            print(f"Data: {data}")
+
+            if data == "O":
+                open_lid()
+                close_lid()
+
+except Exception as e:
+    print("error:", e)
+
+finally:
+    servo.stop()
+    GPIO.cleanup()
+    client_sock.close()
+    server_sock.close()
+```
+
+---
