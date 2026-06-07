9b8ebf699ff097b978f304d0b92748603fe83a88
이선호 <qwer1234sunstar@gmail.com>
2026-06-08 06:15:21 +0900

    딥러닝 모델 수정 및 벡엔드 수정

--- backend/main.py
+++ backend/main.py
@@ -8,13 +8,25 @@ from PIL import Image
 from torchvision import transforms, models
 
 from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 app = FastAPI()
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+
 class_names = ["CAN", "GLASS", "PET"]
+
 
 model = models.mobilenet_v2(weights=None)
 num_features = model.classifier[1].in_features
@@ -26,11 +38,14 @@ try:
     model.load_state_dict(torch.load(MODEL_PATH, map_location=device))
     model = model.to(device)
     model.eval()
-    print("AI Trash Model loaded successfully.")
+    print("==================================================")
+    print("✅ AI Trash Model loaded successfully.")
+    print("==================================================")
 except FileNotFoundError:
-    print(f"Error: {MODEL_PATH} not found in backend folder.")
+    print(f"❌ Error: {MODEL_PATH} not found in backend folder.")
 except Exception as e:
-    print(f"Error loading AI model: {e}")
+    print(f"❌ Error loading AI model: {e}")
+
 
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
@@ -64,10 +79,16 @@ def health_check():
     }
 
 
+
 @app.post("/predict")
 async def predict_trash(file: UploadFile = File(...)):
     try:
         image_bytes = await file.read()
+
+
+        with open("check_pad_image.jpg", "wb") as f:
+            f.write(image_bytes)
+
         image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
 
         input_tensor = transform(image).unsqueeze(0).to(device)
@@ -78,7 +99,16 @@ async def predict_trash(file: UploadFile = File(...)):
             confidence, preds = torch.max(probabilities, 1)
 
             label = class_names[preds.item()]
+
+
             score = float((1.0 - confidence.item()) * 30.0)
+
+
+            prob_list = [round(p * 100, 2) for p in probabilities.tolist()[0]]
+            print(f"\n[AI 실시간 연산 분석 정보]")
+            print(f"▶️ 전체 확률 분포 (CAN, GLASS, PET 순서): {prob_list}%")
+            print(f"▶️ 선택된 인덱스 번호: {preds.item()} | 매칭된 라벨: {label}")
+            print(f"▶️ 확신도(Confidence): {round(confidence.item() * 100, 2)}% | 계산된 오염도: {round(score, 2)}%\n")
 
         return {
             "label": label,
@@ -86,6 +116,7 @@ async def predict_trash(file: UploadFile = File(...)):
         }
 
     except Exception as e:
+        print(f"🚨 서버 내부에러 발생: {str(e)}")
         raise HTTPException(
             status_code=500,
             detail=f"AI analysis server error: {str(e)}"
